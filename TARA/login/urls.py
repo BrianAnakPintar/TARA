4cfc154f8cc23efff2a63bcb768490d405467f8c
Bobby Suciady <suciadywilliam@gmail.com>
2022-03-22 22:27:17 +0800

    Merged login and teacherrater

--- TARA/login/urls.py
+++ TARA/login/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.loginpage, name="login"),
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path("logout", views.logoutuser, name="logout")
 ]
