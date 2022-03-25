d3b6f6914c4cfc4cf9f028a620fe2afdc1cc1590
Bobby Suciady <suciadywilliam@gmail.com>
2022-03-25 09:14:06 +0800

    Account activation added

--- TARA/login/urls.py
+++ TARA/login/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.loginpage, name="login"),
     path("register/", views.register, name="register"),
-    path("logout", views.logoutuser, name="logout")
+    path("logout", views.logoutuser, name="logout"),
+    path('token/', views.token_send, name="token_send"),
+    path('verify/<auth_token>', views.verify, name="verify"),
+    path('error/', views.error_page, name="error")
 ]
--- TARA/login/views.py
+++ TARA/login/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from django.shortcuts import redirect, render
 from django.contrib import messages
+from .models import *
+import uuid
+from django.conf import settings
+from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login, logout
-from django.db import IntegrityError
+
+
+def send_mail_after_registration(email, token):
+    subject = 'Your accounts need to be verified'
+    message = 'Hi paste the link to verify your account http://127.0.0.1:8000/login/verify/'+token
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list,  fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None)
+
 
 # login page
 def loginpage(request):
@@ -10,33 +21,93 @@ def loginpage(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("index")
-        else:
-            messages.info(request, "Username or Password is incorrect")
+        user_obj = User.objects.filter(username=username).first()
+        if user_obj is None:
+            messages.success(request, 'User not found.')
+            return redirect('login')
+
+        profile_obj = Profile.objects.filter(user=user_obj).first()
+
+        if not profile_obj.is_verified:
+            messages.success(request, 'Profile is not verified check your mail.')
+            return redirect('login')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            messages.success(request, 'Wrong password.')
+            return redirect('login')
+
+        login(request, user)
+        return redirect('index')
 
     return render(request, 'login/login.html')
 
 
-# register page
 def register(request):
-    form = CreateUserForm()
-    isEmailtaken = False
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save() #make account
-                messages.success(request, "Account successfully made")
-                return redirect("login")
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
 
-            except IntegrityError:
-                isEmailtaken = True
-                return render(request, 'login/register.html', {"form": form, "emailerror":isEmailtaken}) # email error
+    try:
+        if User.objects.filter(username = username).first():
+            messages.success(request, "Username is taken")
+            return redirect(request, 'login/register.html')
+        if User.objects.filter(email = email).first():
+            messages.success(request, "Email is taken")
+            return redirect(request, 'login/register.html')
 
-    return render(request, 'login/register.html', {"form": form, "emailerror": isEmailtaken})  # run main function
+        user_obj = User(username=username, email=email)
+        user_obj.set_password(password)
+        user_obj.save()
+
+        auth_token = str(uuid.uuid4())
+        profile_obj = Profile.objects.create(user=user_obj, auth_token=auth_token)
+        profile_obj.save()
+        send_mail_after_registration(email, auth_token)
+        return redirect('token_send')
+
+    except Exception as e:
+        print(e)
+
+    return render(request, 'login/register.html')
+
+
+
+def token_send(request):
+    return render(request, 'login/token_send.html')
+
+
+def verify(request, auth_token):
+    try:
+        profile_obj = Profile.objects.filter(auth_token=auth_token).first()
+
+        if profile_obj:
+            if profile_obj.is_verified:
+                messages.success(request, 'Your account is already verified.')
+                return redirect('login')
+            profile_obj.is_verified = True
+            profile_obj.save()
+            messages.success(request, 'Your account has been verified.')
+            return redirect('login')
+        else:
+            return redirect('error')
+    except Exception as e:
+        print(e)
+
+        return redirect('login')
+
+
+def error_page(request):
+    return render(request, 'login/error.html')
+
+
+
+
+# register page
+
+
+
+
 
 
 #logout from account
