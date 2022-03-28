2108c032d9a5579c2def476681a4d94ea0081f8c
Bobby Suciady <suciadywilliam@gmail.com>
2022-03-28 15:59:19 +0800

    Reset password added

--- TARA/login/urls.py
+++ TARA/login/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.loginpage, name="login"),
@@ -7,5 +8,14 @@ urlpatterns = [
     path("logout", views.logoutuser, name="logout"),
     path('token/', views.token_send, name="token_send"),
     path('verify/<auth_token>', views.verify, name="verify"),
-    path('error/', views.error_page, name="error")
+    path('error/', views.error_page, name="error"),
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name="login/password_reset.html"), name="reset_password"),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="login/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="login/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="login/password_reset_done.html"), name="password_reset_complete")
+
 ]
+
+"""
+path('forgot_password/' , views.ForgetPassword , name="forgot_password"),
+path('change_password/<token>/', views.ChangePassword , name="change_password"),"""
