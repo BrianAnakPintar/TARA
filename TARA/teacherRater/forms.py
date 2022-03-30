3cb52231ae2d41bf6157e801a99b95dfa5ecc089
Brian <brianfransis.moniaga@gmail.com>
2022-03-30 12:03:21 +0800

    Added rating functions and overall ratings!

--- TARA/teacherRater/forms.py
+++ TARA/teacherRater/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from teacherRater.models import reviews, RatingChoices
+from teacherRater.models import reviews, understandabilityChoices, communicationChoices, teachingMethodChoices
 
 class ratingForms(forms.ModelForm):
     commentsReview = forms.CharField(widget=forms.Textarea, required=False)
-    rate = forms.ChoiceField(choices=RatingChoices, widget=forms.Select(), required=True)
-    makeAnonymous = forms.BooleanField(widget=forms.CheckboxInput)
+    understandability = forms.ChoiceField(choices=understandabilityChoices, widget=forms.Select(), required=True)
+    communication = forms.ChoiceField(choices=communicationChoices, widget=forms.Select(), required=True)
+    teachingMethod = forms.ChoiceField(choices=teachingMethodChoices, widget=forms.Select(), required=True)
+    makeAnonymous = forms.BooleanField(widget=forms.CheckboxInput, required=False)
     class Meta:
         model = reviews
-        fields = ['commentsReview', 'rate', 'makeAnonymous']
+        fields = ['commentsReview', 'understandability', 'communication', 'teachingMethod', 'makeAnonymous']
