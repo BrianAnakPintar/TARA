1b2f8722c4a9c9db3cfef335b6e1af12ae803342
Bobby Suciady <suciadywilliam@gmail.com>
2022-04-01 12:13:36 +0800

    slight changes

--- TARA/login/urls.py
+++ TARA/login/urls.py
@@ -13,9 +13,4 @@ urlpatterns = [
     path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="login/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="login/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="login/password_reset_done.html"), name="password_reset_complete")
-
 ]
-
-"""
-path('forgot_password/' , views.ForgetPassword , name="forgot_password"),
-path('change_password/<token>/', views.ChangePassword , name="change_password"),"""
